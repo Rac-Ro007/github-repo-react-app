86ab990c94b9a59f9888bb1ae4e67e8ad22006c8
Ronak Vadhaiya <ronak.vadhaiya@somaiya.edu>
2024-04-19 17:10:16 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { HashRouter } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+         <Routes>
+            {/* If you find path with 'hello' render element helloWorld */}
+            <Route path="/" element={<Navigate to="/Labs/a3" />} />
+            <Route path="/hello" element="Hello" />
+            <Route path="/Account/*" element="Users" />
+         </Routes>
+      </HashRouter>
   );
 }
 
